9b6bb1ffb53af0d6d126f936795c4b0a5307e31f
DESKTOP-C9HHN7V\A8-PC00 <licencias@svalero.com>
2023-03-08 21:15:08 +0100

    asd

--- calcularNomina.js
+++ calcularNomina.js
@@ -1,17 +1,17 @@
 let salarioBase, pagasExtra
-let pee
+let pee, bccc
 
 document.getElementById("enviarBoton").onclick = function () {
     salarioBase = document.getElementById("salarioBase").value
     pagasExtra = document.getElementById("pagasExtra").value
     pee = (salarioBase * pagasExtra) / 12
-    mostrarResultado(pee)
+    bccc = salarioBase + pee
+    mostrarResultado(pee, bccc)
 }
 
-function mostrarResultado(s_cotizado){
+function mostrarResultado(s_pee, s_bccc){
     const para = document.createElement("p");
-    const node = document.createTextNode("PPE: " + s_cotizado);
-    para.appendChild(node);
+    para.appendChild(document.createTextNode("PPE: " + s_pee  + " - " + " BCCC: " + s_bccc));
     const element = document.getElementById("resultadoDiv");
     element.appendChild(para);
 }
